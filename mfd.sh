59ebd6edb2359d8d32a94e5249e487550f0eb029
lowryLin <linqi515555904@gmail.com>
2016-10-09 21:27:42 +0800

    new mfd version in android5.1

---
+++ mfd.sh
@@ -0,0 +1,12 @@
+#！/bin/bash
+ndk-build NDK_PROJECT_PATH=./MultiFrameDenoise/ NDK_APPLICATION_MK=MultiFrameDenoise/Application.mk APP_BUILD_SCRIPT:=MultiFrameDenoise/Android.mk
+# adb root
+# adb remount
+# adb shell input keyevent 3
+# adb push libs/armeabi/libMFDenoise.so /system/lib
+# adb shell stop media
+# adb shell start media
+# sleep 1
+# adb shell am start -n com.android.camera2/com.android.camera.CameraLauncher
+# adb logcat -s mfdenoise
+# adb logcat -c
